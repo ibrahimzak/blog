35c35b58508c48e74bf072f28ef39e2e009f586d
Ibrahim Zakarya <ibrahim_68657@svuonline.org>
2018-12-17 19:21:07 +0200

    fix dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -52,7 +52,7 @@
                 scales: {
                     yAxes: [{
                         ticks: {
-                            beginAtZero: false
+                            beginAtZero: true
                         }
                     }]
                 },
