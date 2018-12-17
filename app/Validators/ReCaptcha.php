5241c92803ab56b6c224023136dc1df68d426447
Ibrahim Zakarya <ibrahim_68657@svuonline.org>
2018-12-17 07:35:16 +0200

    recaptcha

---
+++ app/Validators/ReCaptcha.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use GuzzleHttp\Client;
+
+
+use Illuminate\Support\Facades\Validator;
+
+class ReCaptcha
+{
+    public function validate(
+        $attribute,
+        $value,
+        $parameters,
+        $validator
+    ){
+
+        $client = new Client();
+
+        $response = $client->post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            ['form_params'=>
+                [
+                    'secret'=>env('GOOGLE_RECAPTCHA_SECRET'),
+                    'response'=>$value
+                ]
+            ]
+        );
+
+        $body = json_decode((string)$response->getBody());
+        return $body->success;
+    }
+
+}
