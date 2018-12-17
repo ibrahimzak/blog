67e9adbadf3b60552b6400e12f375fc59ff17dc1
Ibrahim Zakarya <ibrahim_68657@svuonline.org>
2018-12-17 19:48:57 +0200

    category show fix

--- resources/views/layouts/categories.blade.php
+++ resources/views/layouts/categories.blade.php
@@ -1,7 +1,7 @@
 <div class="nav-scroller py-1 mb-2">
     <nav class="nav d-flex justify-content-between" style="justify-content: center !important;">
         @foreach($categories as $category)
-            <a class="p-2 text-muted" href="#">{{$category->name}}</a>
+            <a class="p-2 text-muted" href="/category/{{$category->id}}">{{$category->name}}</a>
         @endforeach
     </nav>
 </div>
